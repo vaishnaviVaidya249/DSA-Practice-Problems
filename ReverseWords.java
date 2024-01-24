a19a3550f81b690ccce07845a12f4e5d73c37cdd
Vaishnavi <vaishnavi.vaidya249@gmail.com>
2024-01-24 23:38:51 +0530

    commit

--- ReverseWords.java
+++ ReverseWords.java
@@ -1,5 +1,7 @@
 public class ReverseWords {
-    String[] a=str.split(" ");
+    public static void main(String[] args) {
+        String str="i am learning";
+        String[] a=str.split(" ");
 		StringBuilder sb=new StringBuilder();
 	    for(int i=a.length-1;i>=0;i--){
 
@@ -14,5 +16,35 @@ public class ReverseWords {
         }
 		
 		
-		return sb.toString();
+		System.out.println(sb.toString());
+    }
 }
+        
+    
+		
+		
+	
+    
+// String input="I am Learning";
+//         char arr[]=input.toCharArray();
+//         List<Character> l=new ArrayList<>();
+//         for(char i:arr){
+//             l.add(i);
+//         }
+//         Collections.reverse(l);
+//         ListIterator it=l.listIterator();
+//         while(it.hasNext()){
+//             System.out.print(it.next());
+//         }
+        
+        //String arr[]=input.split(" ");
+       
+        
+        // for(int i=arr.length-1;i>0;i--){
+        //     res+=arr[i]+" ";
+            
+        // }
+        // System.out.println(res+arr[0]);
+     
+        
+        
--- duplicates.java
+++ duplicates.java
@@ -22,6 +22,20 @@ class Solution {
          }
          nums[j]=nums[nums.length-1];
          return j+1;
+
+
+
+        //  int arr[]={10,13,10,26,10,26,8};
+        // List<Integer> l=new ArrayList<>();
+        // for(int i=0;i<arr.length;i++){
+        //     if(!l.contains(arr[i])){
+        //         l.add(arr[i]);
+        //     }
+        // }
+        // Integer[] res=new Integer[l.size()];
+        // res=l.toArray(res);
+        // for(int i=0;i<res.length;i++){
+        //     System.out.println(res[i]);
         
 
             
