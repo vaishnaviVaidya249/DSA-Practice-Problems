2fd8d0deb57469aa87e7d1ed10b20451b51498bf
Vaishnavi <vaishnavi.vaidya249@gmail.com>
2024-02-01 00:19:34 +0530

    commit

--- Preorder.java
+++ Preorder.java
@@ -72,11 +72,49 @@ public static void  PostOrderTraversal(Node root){
     PostOrderTraversal(root.right);
     System.out.print(root.data+" ");
    }
+
+
+
 public static void levelOrder(Node root){
+    if(root==null){
+        return;
+    }
     Queue<Node> q=new LinkedList<>();
     q.add(root);
     q.add(null);
+
+    while(!q.isEmpty()){
+        Node currNode=q.remove();
+        if(currNode==null){
+            System.out.println();
+            if(q.isEmpty()){
+                break;
+            }else{
+                q.add(null);
+            }
+
+        }else{
+            System.out.print(currNode.data+" ");
+            if(currNode.left!=null){
+                q.add(currNode.left);
+            }
+            if(currNode.right!=null){
+                q.add(currNode.right);
+            }
+        }
+    }
     
+}
+
+public static int countOfNodes(Node root){
+    if(root==null){
+        return 0;
+    }
+    int leftNodes=countOfNodes(root.left);
+    int rightNodes=countOfNodes(root.right);
+
+    return leftNodes+rightNodes+1;
+
 }
 
 
@@ -90,6 +128,9 @@ public static void levelOrder(Node root){
         InOrderTraversal(root);
         System.out.println();
         PostOrderTraversal(root);
+        System.out.println("----");
+        levelOrder(root);
+        System.out.println("count of nodes is "+countOfNodes(root));
 
         
     }
