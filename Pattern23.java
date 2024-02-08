a51b5f1ec5f6ceaf542a793bed75c83cd4b955e0
Vaishnavi <vaishnavi.vaidya249@gmail.com>
2024-02-08 13:28:54 +0530

    commit

---
+++ Pattern23.java
@@ -0,0 +1,15 @@
+class Pattern23{
+    public static void main(String[] args){
+        for(int i=1;i<=4;i++){
+            for(int j=4;j>=i;j--){
+                System.out.print(" ");
+                
+            }
+            for(int j=1;j<=4;j++){
+                System.out.print("* ");
+                
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ Pattern24.java
@@ -0,0 +1,36 @@
+public class Pattern24 {
+    public static void main(String[] args){
+
+
+
+        // for(int i=0;i<=4;i++){
+
+        //     for(int j=4;j>=i;j--){
+        //         System.out.print("  ");
+        //     }
+        //      for(int j=1;j<=(2*i)+1;j++){
+        //          System.out.print("* ");
+        //      }
+        //      System.out.println();
+            
+        // }
+
+        int y=1;
+        for(int i=0;i<5;i++){
+            int z=y;
+            for(int j=0;j<=i;j++){
+                System.out.print(z+" ");
+
+                if(j%2==0){
+                    z=z+2*(5-i)-1;
+                }else{
+                    z=z+2*(i-1);
+                }
+            }
+            y=y+1;
+            System.out.println();
+        }
+
+    }
+    
+}
