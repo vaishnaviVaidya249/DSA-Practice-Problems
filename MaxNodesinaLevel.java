efb7a2dcd26a7d3b4b3a12db6dc15e4d203b23a5
Vaishnavi <vaishnavi.vaidya249@gmail.com>
2024-03-06 00:30:23 +0530

    commit

---
+++ MaxNodesinaLevel.java
@@ -0,0 +1,12 @@
+public class MaxNodesinaLevel {
+    public class Solution {
+        public static int numberOfNodes(int N){
+    
+            return (int)Math.pow(2,N-1);
+            
+    
+        
+        }
+    }
+    
+}
